743d28165cfd0e62b2ec6312afc9d06db88d02ad
Ubuntu <doanhoaison97@gmail.com>
2024-04-18 16:50:59 +0700

    chore: add .env file

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      script: 'bin/www', // the path of the script you want to execute,
+      name: 'development',
+      env: {
+        NODE_ENV: 'development',
+      }
+    },
+    {
+      script: 'bin/www', // the path of the script you want to execute,
+      name: 'production',
+      env: {
+        NODE_ENV: 'production',
+    },
+    }
+  ],
+};
