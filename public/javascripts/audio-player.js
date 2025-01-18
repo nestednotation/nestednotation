58ad6dd03d8f183ec941f93e67244c45b51af99e
Tai Le <thanhtai08021998@gmail.com>
2025-01-19 03:43:53 +0700

    refactor(session): cleanup code for audio player

--- public/javascripts/audio-player.js
+++ public/javascripts/audio-player.js
@@ -1,3 +1,5 @@
+const SESSION_PLAY_MODES = { NOTE: "NOTE", CHORD: "CHORD", MUTE: "MUTE" };
+
 const removeFileExt = (fileName) => {
   const splitted = fileName.split(".");
 
@@ -103,8 +105,8 @@ class Frame {
   notes = [];
   soundMap = {};
 
-  constructor(id, notes) {
-    this.notes = notes;
+  constructor(id, frameElement, globalSoundMap) {
+    this.notes = this.getFrameNotes(frameElement, globalSoundMap);
     this.id = id;
     this.soundMap = this.getSoundMap();
   }
@@ -130,58 +132,84 @@ class Frame {
       for (const s of curr.sounds) {
         acc[s.name] = s;
       }
+
       return acc;
     }, {});
   }
+
+  getFrameNotes(frame, globalSoundMap) {
+    const notes = [];
+
+    const svgSoundNodes = frame.querySelectorAll("[sound]");
+    for (const svgNode of svgSoundNodes) {
+      const soundNames = svgNode.getAttribute("sound")?.split(",");
+      if (!soundNames) {
+        return;
+      }
+
+      const volumes = svgNode
+        .getAttribute("volume")
+        ?.split(",")
+        .map(Number) ?? [DEFAULT_VOLUME];
+      const autoPlay = JSON.parse(svgNode.getAttribute("autoplay") ?? true);
+      const sounds = soundNames.map((name) => {
+        const soundInstant = globalSoundMap[name];
+        soundInstant.name = name;
+        return soundInstant;
+      });
+
+      const note = new Note(mergeSoundsAndVolumes(sounds, volumes), autoPlay);
+
+      note.parent = this;
+      notes.push(note);
+      svgNode.addEventListener("click", () => {
+        note.toggleSound();
+      });
+    }
+
+    return notes;
+  }
 }
 
-class SessionAudio {
+class AudioSession {
   currFrameId = null;
 
+  // Map of sound name to Howl instance
   soundMap = {};
+  // Map of frame id to Frame instance
   frameMap = {};
 
-  frames = [];
+  mode = SESSION_PLAY_MODES.NOTE;
 
-  mode = "note";
-  noteModeEnabled = true;
-  chordModeEnabled = false;
-
-  soundEnabled = true;
+  autoPlay = false;
 
   init() {
     const modeButton = document.getElementById("mode-btn");
-    const modeStep = { note: "chord", chord: "mute", mute: "note" };
 
     modeButton.addEventListener("click", () => {
-      // 3 mode: note, chord, mute
+      // 3 mode: NOTE, CHORD, MUTE
+      // The cylce is: NOTE -> CHORD -> MUTE -> NOTE
       const currMode = modeButton.dataset.mode;
-      const nextMode = modeStep[currMode];
-
-      modeButton.dataset.mode = nextMode;
-      this.handleChangeMode(nextMode);
+      switch (currMode) {
+        case SESSION_PLAY_MODES.NOTE: {
+          modeButton.dataset.mode = SESSION_PLAY_MODES.CHORD;
+          this.frameMap[this.currFrameId]?.playAll();
+          break;
+        }
+        case SESSION_PLAY_MODES.CHORD: {
+          modeButton.dataset.mode = SESSION_PLAY_MODES.MUTE;
+          this.frameMap[this.currFrameId]?.stopAll();
+          break;
+        }
+        case SESSION_PLAY_MODES.MUTE:
+        default: {
+          modeButton.dataset.mode = SESSION_PLAY_MODES.NOTE;
+          break;
+        }
+      }
     });
 
     this.loadSounds();
-  }
-
-  handleChangeMode(mode) {
-    switch (mode) {
-      case "note": {
-        this.mode = "note";
-        break;
-      }
-      case "chord": {
-        this.frameMap[this.currFrameId]?.playAll();
-        this.mode = "chord";
-        break;
-      }
-      case "mute": {
-        this.mode = "mute";
-        this.frameMap[this.currFrameId]?.stopAll();
-        break;
-      }
-    }
   }
 
   loadSounds() {
@@ -204,46 +232,21 @@ class SessionAudio {
 
   generateSoundMap() {
     const mainSvgContainer = document.getElementById("MainSVGContent");
+    if (!mainSvgContainer) {
+      console.error("MainSVGContent not found");
+      return;
+    }
+
     const frameSvg = mainSvgContainer.querySelectorAll("svg[id]");
+    if (frameSvg.length === 0) {
+      console.error("No frame found");
+      return;
+    }
 
     for (const frame of frameSvg) {
-      const notes = this.extractFrameNotes(frame);
-      const frameInstance = new Frame(frame.id, notes);
+      const frameInstance = new Frame(frame.id, frame, this.soundMap);
       this.frameMap[frame.id] = frameInstance;
-      notes.forEach((n) => (n.parent = frameInstance));
     }
-  }
-
-  extractFrameNotes(frame) {
-    const notes = [];
-
-    const svgSoundNodes = frame.querySelectorAll("[sound]");
-    for (const svgNode of svgSoundNodes) {
-      const soundNames = svgNode.getAttribute("sound")?.split(",");
-      if (!soundNames) {
-        return;
-      }
-
-      const volumes = svgNode
-        .getAttribute("volume")
-        ?.split(",")
-        .map(Number) ?? [DEFAULT_VOLUME];
-      const autoPlay = JSON.parse(svgNode.getAttribute("autoplay") ?? true);
-      const sounds = soundNames.map((name) => {
-        const soundInstant = this.soundMap[name];
-        soundInstant.name = name;
-        return soundInstant;
-      });
-
-      const note = new Note(mergeSoundsAndVolumes(sounds, volumes), autoPlay);
-
-      notes.push(note);
-      svgNode.addEventListener("click", () => {
-        note.toggleSound();
-      });
-    }
-
-    return notes;
   }
 
   handleChangeFrame(nextFrameId) {
@@ -259,7 +262,7 @@ class SessionAudio {
 
     const { fadeDuration = 1000 } = window;
 
-    if (this.mode === "chord") {
+    if (this.mode === SESSION_PLAY_MODES.CHORD) {
       const prevSoundData = this.frameMap[prevId].soundMap;
       const nextSoundData = this.frameMap[nextId].soundMap;
 
@@ -303,7 +306,7 @@ class SessionAudio {
       });
     }
 
-    if (this.mode === "note") {
+    if (this.mode === SESSION_PLAY_MODES.NOTE) {
       const prevSoundData = this.frameMap[prevId].soundMap;
       const nextSoundData = this.frameMap[nextId].soundMap;
 
@@ -366,7 +369,7 @@ class SessionAudio {
   }
 }
 
-const audioPlayer = new SessionAudio();
+const audioPlayer = new AudioSession();
 
 window.audioPlayer = audioPlayer;
 
@@ -378,7 +381,7 @@ const handleOnUpdateView = ({ detail }) => {
     Howler.mute(true);
     return;
   } else {
-    Howler.mute(!audioPlayer.soundEnabled);
+    Howler.mute(false);
   }
 
   const frameId = `svg${newIndex}`;
