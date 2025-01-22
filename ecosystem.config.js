a4ee1dbe5e16755bd6acb61fd834ed8bbc228c45
Sơn Đoàn Hoài <32816961+dhoaison@users.noreply.github.com>
2025-01-22 12:57:04 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,18 @@ module.exports = {
       name: 'development',
       env: {
         NODE_ENV: 'development',
+        PORT:3001,
+        WS_PORT:2382,
+        SERVER_IP:'devapi.nestednotation.com'
       },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT:3000,
+        WS_PORT:2381,
+        SERVER_IP:'proapi.nestednotation.com'
+      }
+    }
       
-    },
+    
   ],
 };
