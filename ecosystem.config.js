6911f41124b2aaf7d38b39d226c3454bd344ca7b
dhoaison <doanhoaisonfithcmus@gmail.com>
2025-01-22 04:20:34 +0000

    build: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,16 @@ module.exports = {
       name: 'development',
       env: {
         NODE_ENV: 'development',
+	PORT:3001,
+	WS_PORT:2382,
+	SERVER_IP:'devapi.nestednotation.com'
       },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT:3000,
+        WS_PORT:2381,
+        SERVER_IP:'proapi.nestednotation.com'
+      },,
       
     },
   ],
